14759cd8ca4922ecd0601f059399c85f304359d7
Jerry8块 <i@jerryzone.cn>
2024-08-03 21:30:15 +0800

    remote change to 4

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main(){
-		fmt.Println("Hello world! change1")
+		fmt.Println("Hello world! change4")
 		fmt.Println("ChangeREmote!")
 		fmt.Println("ChangeREmote!")
 }
